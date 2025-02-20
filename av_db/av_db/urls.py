44772bc40892a2785240655ed67e0c8bc7e1299a
ChoiByuengJun <gs3687@naver.com>
2025-02-20 15:19:16 +0900

    feat: [최병준] 데이터 불러오기 [AV-x]

--- av_db/av_db/urls.py
+++ av_db/av_db/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for av_db project.
+URL configuration for AIM_Sniper_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,10 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('kakao_oauth/', include('kakao_oauth.urls')),
+    path('account/', include('account.urls')),
+    path('survey/', include('survey.urls')),
+    path('google_oauth/', include('google_oauth.urls')),
+    path('naver_oauth/', include('naver_oauth.urls')),
+    path('company_report/',include('company_report.urls')),
+    path('cart/',include('cart.urls')),
+    path('orders/',include('orders.urls')),
+    path('marketing/',include('marketing.urls')),
+    path('management/',include('management.urls')),
+    path('interview/', include('interview.urls')),
+    path('interview_result/', include('interview_result.urls'))
 ]
