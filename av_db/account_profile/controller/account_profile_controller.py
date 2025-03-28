7e7a2a6574748c26d94c9185bb232f374d37cfbb
의정 Ui Jung, Yang <5012jung@gmail.com>
2025-03-28 10:22:47 +0900

    feat: [양의정] MyPage 기능 구현 (non-funtional) [AIV-3]

--- av_db/account_profile/controller/account_profile_controller.py
+++ av_db/account_profile/controller/account_profile_controller.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework import viewsets, status
 
+from account_profile.entity.account_profile import AccountProfile
 from account_profile.service.account_profile_service_impl import AccountProfileServiceImpl
 from redis_cache.service.redis_cache_service_impl import RedisCacheServiceImpl
 
@@ -9,42 +10,55 @@ class AccountProfileController(viewsets.ViewSet):
     __accountProfileService = AccountProfileServiceImpl.getInstance()
     redisCacheService = RedisCacheServiceImpl.getInstance()
 
-
     def requestInfo(self, request):
         postRequest = request.data
         userToken = postRequest.get("userToken")
 
         if not userToken:
-            return JsonResponse({"error": "userToken이 없습니다.", "success": False}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({"error": "userToken이 필요합니다"}, status=400)
 
         try:
-            # Redis에서 userToken에 해당하는 accountId를 가져옴
             accountId = self.redisCacheService.getValueByKey(userToken)
-            print(f"accountId 나옴! : {accountId} ")
-
             if not accountId:
-                # Redis에서 accountId를 찾지 못한 경우
-                return JsonResponse({"error": "유효한 userToken이 아닙니다", "success": False}, status=status.HTTP_404_NOT_FOUND)
+                return JsonResponse({"error": "유효하지 않은 userToken입니다"}, status=404)
 
-            # accountId를 사용하여 이메일을 찾음
+            # 서비스에서 데이터 가져오기
             foundEmail = self.__accountProfileService.findEmail(accountId)
-            foundNickname = self.__accountProfileService.findNickname(accountId)
+            #foundRoleType = self.__accountProfileService.findRoleType(accountId)
+            foundNickName = self.__accountProfileService.findNickname(accountId)
             foundGender = self.__accountProfileService.findGender(accountId)
             foundBirthyear = self.__accountProfileService.findBirthyear(accountId)
 
-            #if foundEmail is None:
-                # 이메일을 찾지 못한 경우
-            #    return JsonResponse({"error": "이메일을 찾을 수 없습니다", "success": False}, status=status.HTTP_404_NOT_FOUND)
-
-            # 이메일을 찾았으면 200 OK 응답
             return JsonResponse({
                 "email": foundEmail,
+               # "roleType": foundRoleType,
+                "nickname": foundNickName,
                 "gender": foundGender,
                 "birthyear": foundBirthyear,
-                "nickname" : foundNickname
-            }, status=status.HTTP_200_OK)
+            }, status=200)
 
         except Exception as e:
-            # 예외 처리
-            print(f"서버 오류 발생: {e}")
-            return JsonResponse({"error": "서버 내부 오류", "success": False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse({"error": str(e)}, status=500)
+
+
+
+
+
+    '''''
+    def request_info(self, request):
+        # 인증된 사용자를 가져옵니다.
+        user = request.user
+        try:
+            profile = AccountProfile.objects.get(account=user)
+            print(f"DB에서 발견한 profile: {profile}")
+        except AccountProfile.DoesNotExist:
+            print("AccountProfile DB에서 정보 못찾음")
+            return JsonResponse({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        return JsonResponse({
+            "email": profile.email,
+            "nickname": profile.nickname,
+            "gender": profile.gender,
+            "birthyear": profile.birthyear,
+        }, status=status.HTTP_200_OK)
+'''
