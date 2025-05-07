069f13adba80138fd378fdd076b6e7bfdb3c19fe
의정 Ui Jung, Yang <5012jung@gmail.com>
2025-05-07 16:03:13 +0900

    fix: [양의정] 인터뷰 정보에 companyName으로 수정 [AIV-32]

--- av_db/interview/entity/interview_company.py
+++
@@ -1,9 +0,0 @@
-# interview/entity/interview_company.py
-
-from django.db.models import TextChoices
-
-class InterviewCompany(TextChoices):
-    DANGGEUN = "danggeun", "당근마켓"
-    TOSS = "toss", "Toss"
-    SK_ENCORE = "sk_encore", "SK엔코아"
-    KT_MOBILE = "kt_mobile", "KT모바일"
