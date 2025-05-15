964fbb705fcfa25de55d3ad1d95ff55117f86d74
왕환민 <mini_wang@naver.com>
2025-05-15 09:25:44 +0900

    Update requirements.txt

--- av_db/review/entity/review.py
+++ av_db/review/entity/review.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-from account_profile.entity.account_profile import AccountProfile
+from account.entity.account import Account
 
 
 class Review(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    writer = models.ForeignKey(AccountProfile, on_delete=models.CASCADE, related_name="blog_posts")
+    writer = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="blog_posts")
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
