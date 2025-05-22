126abf457ec6e50bc65e4609a2baa1e31bf314e0
ChoiByuengJun <gs3687@naver.com>
2025-05-22 16:50:16 +0900

    fix: [최병준] membership db오류 수정[AIV-36]

--- av_db/membership_plan/apps.py
+++ av_db/membership_plan/apps.py
@@ -1,12 +1,11 @@
-# membership_plan/apps.py
-
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 class MembershipConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "membership_plan"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'membership_plan'
 
     def ready(self):
+        print("🔥🔥🔥 ready() 진입 완료")
         from .membership_initializer import create_default_memberships
-        post_migrate.connect(create_default_memberships, sender=self)
+        post_migrate.connect(create_default_memberships)
